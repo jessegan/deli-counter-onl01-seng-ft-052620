c526bd05c4e2211ca599944adc1f00595035cf2e
Jesse Gan <jesse13579@gmail.com>
2020-05-27 15:08:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 def line(line)
   if line.size == 0 
-    return "The line is currently empty."
+    puts "The line is currently empty."
   end 
   
   rtn = "The line is currently: "
   line.each_with_index do |name,i|
     rtn << " #{i+1}. #{name}"
   end
-  rtn
+  puts rtn
 end 
 
 def take_a_number(line,name)
