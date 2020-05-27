45bd3c8c9064cae10ef662600894069829f21cfa
Jesse Gan <jesse13579@gmail.com>
2020-05-27 15:06:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(line)
+  if line.size == 0 
+    return "The line is currently empty."
+  end 
+  
+  rtn = "The line is currently:"
+  line.each_with_index
+end 
+
+def take_a_number(line,name)
+  
+end 
+
+def now_serving(line)
+  
+end
